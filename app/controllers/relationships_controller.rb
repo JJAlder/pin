b01cfee4ab8abb2e11486af56e7d8330c9466336
Jillian Alder <jillianalder@gmail.com>
2015-10-07 13:48:27 -0400

    Fixed some bugs with Relationships and Users. Changed Navbar to have rounded corners

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,21 @@
 class RelationshipsController < ApplicationController
 	before_filter :authenticate_user!
-	
+
+	#def index
+		#@sent_invites = current_user.sent_invites
+    #@received_invites = current_user.received_invites
+	#end
 	def index
-		@sent_invites = Current_user.sent_invites.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
-  	@received_invites = Current_user.received_invites.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 2)
+		@sent_invites = current_user.sent_invites("created_at DESC").paginate(:page => params[:page], :per_page => 5)
+  	@received_invites = current_user.received_invites("created_at DESC").paginate(:page => params[:page], :per_page => 5)
 	end
 
 	def create
     @invited_user = User.find(params[:relationship][:invited_id])
 
-    @relationship = Current_user.sent_invites.build(invited_id: @invited_user.id)
+    @relationship = current_user.sent_invites.build(invited_id: @invited_user.id)
 
-    if @relationship.save
+		if @relationship.save
         flash[:success] = "Successfully invited"
         redirect_to @invited_user
     else
@@ -19,17 +23,13 @@ class RelationshipsController < ApplicationController
         redirect_to
     end
   end
-  def destroy
-    @relationship = Relationship.find(params[:id])
-    if @relationship.inviting_user == current_user
-    	@relationship.destroy
-    	flash[:success] = "Removed relationship"
-    end
-    redirect_to @relationship.invited_user
-	end
 
-	def index
-		@sent_invites = current_user.sent_invites
-    @received_invites = current_user.received_invites
-	end
+  def destroy
+  	@relationship = Relationship.find(params[:id])
+  	if @relationship.inviting_user == current_user
+    	@relationship.destroy
+   	 flash[:success] = "Removed relationship"
+  	end
+  	redirect_to @relationship.invited_user
+	end	
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	def index
-		 @users = User.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 1)
+		 @users = User.all.order("created_at DESC").paginate(:page => params[:page], :per_page => 10)
   end
 	
 	def show
